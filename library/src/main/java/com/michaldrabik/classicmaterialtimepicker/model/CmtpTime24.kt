74bccf61a71a7bfb1765c965240a63b8ee0f682e
Michal <michal.drabik@neoncu.be>
2019-06-05 15:05:07 +0200

    Refactor

--- library/src/main/java/com/michaldrabik/classicmaterialtimepicker/model/CmtpTime24.kt
+++ library/src/main/java/com/michaldrabik/classicmaterialtimepicker/model/CmtpTime24.kt
@@ -1,17 +1,18 @@
 package com.michaldrabik.classicmaterialtimepicker.model
 
 import com.michaldrabik.classicmaterialtimepicker.CmtpTimeData
+import com.michaldrabik.classicmaterialtimepicker.model.CmtpTimeType.HOUR_24
 
 data class CmtpTime24(
   override val hour: Int,
   override val minute: Int,
-  override val type: CmtpTimeType = CmtpTimeType.HOUR_24
+  override val type: CmtpTimeType = HOUR_24
 ) : CmtpTime {
 
   init {
     check(hour in CmtpTimeData.HOURS_24) { "Invalid hour. Must be between 0 and 23" }
     check(minute in CmtpTimeData.MINUTES) { "Invalid minute. Must be between 0 and 59" }
-    check(type == CmtpTimeType.HOUR_24) { "Invalid type. Must be HOUR_12" }
+    check(type == HOUR_24) { "Invalid type. Must be HOUR_12" }
   }
 
   override fun toString() = String.format("%02d:%02d", hour, minute)
