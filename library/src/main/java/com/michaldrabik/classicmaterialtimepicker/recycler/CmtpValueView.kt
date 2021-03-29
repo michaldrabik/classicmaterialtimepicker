65cc2d1e4abe15f6509bd5c881551037ef98eb87
Michal <michal.drabik@neoncu.be>
2021-03-29 14:36:51 +0200

    Default date bugfix

--- library/src/main/java/com/michaldrabik/classicmaterialtimepicker/recycler/CmtpValueView.kt
+++ library/src/main/java/com/michaldrabik/classicmaterialtimepicker/recycler/CmtpValueView.kt
@@ -2,6 +2,8 @@ package com.michaldrabik.classicmaterialtimepicker.recycler
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.FrameLayout
 import com.michaldrabik.classicmaterialtimepicker.R
 import kotlinx.android.synthetic.main.cmtp_value_view.view.*
@@ -12,6 +14,7 @@ internal class CmtpValueView @JvmOverloads constructor(
 
   init {
     inflate(context, R.layout.cmtp_value_view, this)
+    layoutParams = LayoutParams(MATCH_PARENT, WRAP_CONTENT)
   }
 
   fun bind(value: String) {
